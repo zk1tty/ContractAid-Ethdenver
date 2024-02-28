ef7b3534f68ef0e184c07fdc1541d727c7a52188
zkitty <norika@0xcerberus.io>
2024-02-28 16:23:45 -0700

    prNumber:1

--- .github/scripts/commentPR.js
+++ .github/scripts/commentPR.js
@@ -8,21 +8,12 @@ if(process.env.GITHUB_TOKEN){
 
 async function commentOnPR() {
   // Assuming you have the PR number and the repository details
-  const prNumber = process.env.PR_NUMBER;
-  const pr = process.env.PR_EVENT;
+  const prNumber = process.env.PR_NUMBER ? process.env.PR_NUMBER : 1;
   const owner = process.env.REPOSITORY_OWNER;
   const repo = process.env.REPOSITORY_NAME;
 
   const comment = `Comment "PR:${prNumber}, Owner:${owner}, repo:${repo}"`;
 
-  if (!pr) {
-    console.error("Missing PR.");
-    return;
-  }
-  if (pr && !prNumber) {
-    console.error("Missing prNumber", pr);
-    return;
-  }
   try{
     await octokit.issues.createComment({
         owner,
