8475cc65793dab12264413f9c5ff9f73dc7c6be3
zkitty <norika@0xcerberus.io>
2024-02-28 15:02:44 -0700

    add env value try-catch

--- .github/scripts/commentPR.js
+++ .github/scripts/commentPR.js
@@ -2,20 +2,32 @@ const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
+if(process.env.GITHUB_TOKEN){
+    console.log("GITHUB_TOKEN:", process.env.GITHUB_TOKEN);
+}
+
 async function commentOnPR() {
   // Assuming you have the PR number and the repository details
   const prNumber = process.env.PR_NUMBER;
   const owner = process.env.REPOSITORY_OWNER;
   const repo = process.env.REPOSITORY_NAME;
 
-  const comment = "Hello, PR comment";
+  const comment = `Comment "PR:${prNumber}, Owner:${owner}, repo:${repo}"`;
 
-  await octokit.issues.createComment({
-    owner,
-    repo,
-    issue_number: prNumber,
-    body: comment,
-  });
+  if (!prNumber || !owner || !repo) {
+    console.error("Missing required environment variables.");
+    return;
+  }
+  try{
+    await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: prNumber,
+        body: comment,
+      });  
+  }catch(err){
+    console.error(error); 
+  }
 }
 
 commentOnPR().catch((error) => {
