7c7b7f938a3771f409105565445bcabb67942aea
zkitty <norika@0xcerberus.io>
2024-02-29 02:50:40 -0700

    add sampleReport

--- .github/scripts/commentPR.js
+++ .github/scripts/commentPR.js
@@ -13,15 +13,14 @@ async function commentOnPR() {
   const repo = process.env.REPOSITORY_NAME;
 
   const comment = `Comment "PR:${prNumber}, Owner:${owner}, repo:${repo}"`;
-  // Markdown: 
 
-const mdComment = `| Severity | Number | Line of Code     | Link to Code                             |
-|----------|--------|------------------|-------------------------------------------|
-| Critical | 1      | \`const foo = 42;\`| [View Code](https://github.com/user/repo/blob/branch/filename.js#L1) |
-| High     | 2      | \`let bar = foo;\` | [View Code](https://github.com/user/repo/blob/branch/filename.js#L2) |
-| Medium   | 3      | \`if (bar) {...}\` | [View Code](https://github.com/user/repo/blob/branch/filename.js#L3) |
-| Low      | 4      | \`return bar;\`    | [View Code](https://github.com/user/repo/blob/branch/filename.js#L4) |"
-`
+  const mdComment = `| Severity | Number | Line of Code     | Link to Code                             |
+    |----------|--------|------------------|-------------------------------------------|
+    | Critical | 1      | \`const foo = 42;\`| [View Code](https://github.com/user/repo/blob/branch/filename.js#L1) |
+    | High     | 2      | \`let bar = foo;\` | [View Code](https://github.com/user/repo/blob/branch/filename.js#L2) |
+    | Medium   | 3      | \`if (bar) {...}\` | [View Code](https://github.com/user/repo/blob/branch/filename.js#L3) |
+    | Low      | 4      | \`return bar;\`    | [View Code](https://github.com/user/repo/blob/branch/filename.js#L4) |"
+    `
 
 if(!prNumber){
     console.error("Missing", prNumber);
