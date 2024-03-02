ba2c970427b69984226a511d33f3971aaf1c1087
Kevin Wang <38193800+kevin1015wang@users.noreply.github.com>
2024-03-01 18:58:16 -0700

    Got animated progress circle

--- app.js
+++ app.js
@@ -104,6 +104,21 @@ const server = http.createServer(async (req, res) => {
     return; // Important to return here so the code below doesn't execute for CSS requests
   }
 
+  // serve circle-progress.min.js
+  if (parsedUrl.pathname === '/github/circle-progress.min.js') {
+    fs.readFile('./circle-progress.min.js', (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('Not found');
+      }
+      else {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.end(data);
+      }
+    });
+    return; // Important to return here so the code below doesn't execute for CSS requests
+  }
+
   // server linesicon logo
   if (parsedUrl.pathname === '/github/linesicon.png') {
     fs.readFile('./icons/linesicon.png', (err, data) => {
